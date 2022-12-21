7dbf3d304731c5e4485be77a05ab0b119ff155c4
Eduardo <105960444+Eduard0803@users.noreply.github.com>
2022-12-20 21:57:05 -0300

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -8,10 +8,10 @@ typedef int Item;
 void insertion_sort(Item *v, int l, int r)
 {
     int i;
-    for(i=r; i<=l; i--)
+    for(i=r; i>l; i--)
         cmpexch(v[i-1], v[i]);
 
-    for(i=l+1; i<=r; i++){
+    for(i=l+2; i<=r; i++){
         int j=i;Item tmp = v[j];
         while(less(tmp, v[j-1]))
         {v[j] = v[j-1];j--;}
