bb50b0c1d059f2c54d6fb749ac342c65db72cf90
Eduardo <105960444+Eduard0803@users.noreply.github.com>
2023-02-06 21:15:26 -0300

    Update dinamycQueue.h

--- Header's/dinamycQueue.h
+++ Header's/dinamycQueue.h
@@ -21,7 +21,7 @@ int inicializa_fila(fila_st *f)
     return 1;
 }
 
-int enfila(fila_st *f, Item e)
+int enfila(fila_st *f, Item e) // se a fila estiver vazia insere o elemento no inicio, senão insere no final da fila
 {
     no_st *novo_no = malloc(sizeof(no_st));
     if(novo_no == NULL)
@@ -40,7 +40,7 @@ int enfila(fila_st *f, Item e)
     return 1;
 }
 
-Item desenfila(fila_st *f)
+Item desenfila(fila_st *f) // remove o primeiro elemento da fila
 {
     no_st *to_remove = f->primeiro;
     Item ret = to_remove->e;
@@ -50,20 +50,47 @@ Item desenfila(fila_st *f)
     return ret;
 }
 
-Item espia(fila_st *f)
+Item espia(fila_st *f) // retorna o primeiro elemento da fila 
 {
     return f->primeiro->e;
 }
 
-int empty(fila_st *f)
+int empty(fila_st *f) // retorna True se estiver vazia e False senão
 {
     return f->size == 0;
 }
 
-void imprime_fila(no_st *no)
+void imprime_fila(no_st *no) // escreve no console todos os elemento da fila apartir do no recebido
 {
     if(no == NULL)
         return;
     printf("%d\n", no->e);
     imprime_fila(no->prox);
+}
+
+int imprime_fila_in_file(no_st *no, char *file_name) // escreve no arquivo todos os elemento da fila apartir do no recebido
+{
+    FILE *arq = fopen(file_name, "a");
+    if(arq == NULL || no == NULL)
+        return 0;
+    fprintf(arq, "%d\n", no->e);
+    fclose(arq);
+    imprime_fila_in_file(no->prox, file_name);
+}
+
+void inverte_fila(fila_st *f) //inverte a fila, assim o primeiro elemento passa a ser o ultimo e o ultimo o primeiro
+{
+    f->ultimo = f->primeiro;
+    no_st *aux = f->primeiro;
+    no_st *tmp = aux->prox;
+    
+    while(tmp != NULL)
+    {
+        no_st *t = tmp->prox;
+        tmp->prox = aux;
+        aux = tmp;
+        tmp = t;
+    }
+    f->primeiro = aux;
+    f->ultimo->prox = NULL;
 }
