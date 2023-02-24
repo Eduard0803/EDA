761f0ba2b74133cfa3bbf478787b4833207464b8
Eduardo <105960444+Eduard0803@users.noreply.github.com>
2023-02-24 01:30:49 -0300

    Update macros para ordenação.c

--- Sort/macros para ordenação.c
+++ Sort/macros para ordenação.c
@@ -1,6 +1,6 @@
-typedef int Item;
+typedef int Item; // define o tipo 'Item' como inteiro
 
-#define key(a) (a)
-#define less(a, b) (key(a) < key(b))
-#define exch(a, b) {Item t=a;a=b;b=t;}
-#define cmpexch(a, b) {if(less(b, a))exch(a, b)}
+#define key(a) a // retorna a chave do Item 'a'
+#define less(a, b) (key(a) < key(b)) // retorna verdadeiro se a chave de 'a' for menor que a chave de 'b', senão retorna falso
+#define exch(a, b) {Item t=a;a=b;b=t;} // faz a troca entre os Itens 'a' e 'b'
+#define cmpexch(a, b) {if(less(b, a))exch(a, b)} // se a chave de 'b' for menor que a chave de 'a', faz a troca entre os Itens 'a' e 'b'
