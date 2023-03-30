aa47de544b716b2652b6719eb66cf5bcffbd51b7
Eduardo <eduardo83204@gmail.com>
2023-03-30 10:15:08 -0300

    Update merge_sort.h

--- Sort/merge_sort.h
+++ Sort/merge_sort.h
@@ -2,10 +2,8 @@
 
 typedef int Item;
 
-#define key(a) (a)
+#define key(a) a
 #define less(a, b) (key(a) < key(b))
-#define exch(a, b) {Item t=a; a=b; b=t}
-#define cmpexch(a, b) {if(less(b, a)) exch(a, b)}
 
 void merge(Item *v, int l, int m, int r)
 {
